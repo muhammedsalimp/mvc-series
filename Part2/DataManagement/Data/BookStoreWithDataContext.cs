68af2fb906ce7bd72e3b6daf0be522e328244b14
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-05-27 01:35:06 +0530

    Add files via upload

---
+++ Part2/DataManagement/Data/BookStoreWithDataContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using BookStoreWithData.Models;
+
+namespace BookStoreWithData.Data
+{
+    public class BookStoreWithDataContext : DbContext
+    {
+        public BookStoreWithDataContext (DbContextOptions<BookStoreWithDataContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(new Book
+            {
+                Id = 1,
+                Title = "Book1",
+                Genre = "Genre1",
+                Price = 20,
+                PublishDate = new DateTime(2012, 4, 23)
+            }, new Book
+            {
+                Id = 2,
+                Title = "Book2",
+                Genre = "Genre2",
+                Price = 30,
+                PublishDate = new DateTime(2008, 6, 13)
+            });
+        }
+
+        public DbSet<BookStoreWithData.Models.Book> Book { get; set; }
+    }
+}
