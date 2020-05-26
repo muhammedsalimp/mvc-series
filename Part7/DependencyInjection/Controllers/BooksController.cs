1d00b773a0ef5c8736765b4a333d83f94f68b950
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-05-27 01:55:21 +0530

    Add files via upload

---
+++ Part7/DependencyInjection/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Part7/DependencyInjection/Models/DataManager/EmployeeManager.cs
@@ -0,0 +1,21 @@
+﻿using DependencyInjection.Models.Repository;
+using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection.Models.DataManager
+{
+	public class EmployeeManager : IDataRepository<Employee>
+	{
+		public void Add(Employee employee)
+		{
+			throw new NotImplementedException();
+		}
+
+		IEnumerable<Employee> IDataRepository<Employee>.GetAll()
+		{
+			return new List<Employee>() {
+				new Employee()
+			};
+		}
+	}
+}
