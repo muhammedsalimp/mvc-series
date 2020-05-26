10201a0a573c1b4e72db751d108a45e9973bff91
Muhammed Saleem P <muhammedsalimp@gmail.com>
2020-05-27 01:44:45 +0530

    Add files via upload

---
+++ Part4/StateManagement/Controllers/TempDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorkingWithStateManagement.Controllers
+{
+    public class TempDataController : Controller
+    {
+        public IActionResult First()
+        {
+            TempData["UserId"] = 101;
+            return View();
+        }
+
+        public IActionResult Second()
+        {
+            var userId = TempData["UserId"] ?? null;
+            return View();
+        }
+
+        public IActionResult Third()
+        {
+            var userId = TempData["UserId"] ?? null;
+            return View();
+        }
+    }
+}
